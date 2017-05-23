dd6451d6ccac8aa686e1a8bda3104d2a8919ffe5
Roccsa01 <bihal.roland@gmail.com>
2017-05-23 13:17:25 +0200

    E2e tesztesetek

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,4 +1,6 @@
+import { browser, element, by } from 'protractor';
 import { NgTrainingPage } from './app.po';
+
 
 describe('ng-training App', () => {
   let page: NgTrainingPage;
@@ -7,8 +9,31 @@ describe('ng-training App', () => {
     page = new NgTrainingPage();
   });
 
-  it('should display message saying app works', () => {
+  it('should display message saying home works', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('app works!');
+    expect(page.getParagraphText()).toEqual('home works!');
+  });
+});
+
+describe('Bejelentkezés',() => {
+  let page: NgTrainingPage; {
+    var Loginmainbutton = element(by.id('loginn'));
+    var Email = element(by.id('emailke'));
+    var Password = element(by.id('passwordke'));
+    var LoginButton = element(by.id('login'));
+    var username = element(by.id('user'));
+    }
+it('Bejelentkezés,felhasználó ellenőrzés:', function() {
+
+    Loginmainbutton.click();
+    Email.sendKeys('roland.bihal@icellmobilsoft.hu');
+    Password.sendKeys('Aa123456');
+
+    LoginButton.click();
+    expect(username.getText()).toEqual('akármi');
+    element(by.id('user')).getText().then(function(text) {
+    console.log(text);
+    });
+   
   });
 });
--- e2e/app.po.ts
+++ e2e/app.po.ts
@@ -6,6 +6,8 @@ export class NgTrainingPage {
   }
 
   getParagraphText() {
-    return element(by.css('app-root h1')).getText();
+    return element(by.css('app-home')).getText();
   }
+
+
 }
